e3d12990f5797cfc0b09dc1be28b45e8df6a40b2
chiwawaweb <git@chiwawaweb.com>
2018-02-13 12:42:11 +0100

    Maj

---
+++ Forever/DAL/Context.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Forever.DTO;
+using System.Data.Entity;
+
+namespace Forever.DAL
+{
+    public class Context
+    {
+
+    }
+}
---
+++ Forever/DTO/Order.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Forever.DTO
+{
+    public class Order
+    {
+        public int? Id { get; set; }
+        public DateTime Date { get; set; }
+
+    }
+}
