fc8d6c94fdeb81464ca0133980e2676d3492bd8b
chiwawaweb <git@chiwawaweb.com>
2018-02-14 15:29:35 +0100

    Maj

--- Forever/DAL/OrderProvider.cs
+++ Forever/DAL/OrderProvider.cs
@@ -25,6 +25,95 @@ namespace Forever.DAL
             }
         }
 
+        public List<Order> Search(string keywords, string type)
+        {
+            using (Context context = new Context())
+            {
+                try
+                {
+                    var contacts = from b in context.Orders
+                                   orderby b.Date descending
+                                   where (
+                                          (b.Nom.Contains(keywords))
+                                            || (b.Prenom.Contains(keywords))
+                                            || (b.Notes.Contains(keywords))
+                                          )
+                                   select b;
+                    return contacts.ToList();
+                }
+                catch
+                {
+                    throw;
+                }
+            }
+        }
 
+        public int? Create(Order Order)
+        {
+            using (Context context = new Context())
+            {
+                try
+                {
+                    context.Orders.Add(Order);
+                    context.SaveChanges();
+                    int? id = Order.Id;
+                    return id;
+                }
+                catch
+                {
+                    throw;
+                }
+            }
+        }
+
+        public Order GetOrderById(int ID)
+        {
+            using (Context context = new Context())
+            {
+                try
+                {
+                    return context.Orders.Find(ID);
+                }
+                catch
+                {
+                    throw;
+                }
+            }
+        }
+
+        public void Update(Order order)
+        {
+            using (Context context = new Context())
+            {
+                try
+                {
+                    context.Orders.Attach(order);
+                    context.Entry(order).State = EntityState.Modified;
+                    context.SaveChanges();
+                }
+                catch
+                {
+                    throw;
+                }
+            }
+        }
+
+        public void Delete(Order order)
+        {
+            using (Context context = new Context())
+            {
+
+                try
+                {
+                    context.Orders.Attach(order);
+                    context.Orders.Remove(order);
+                    context.SaveChanges();
+                }
+                catch
+                {
+                    throw;
+                }
+            }
+        }
     }
 }
